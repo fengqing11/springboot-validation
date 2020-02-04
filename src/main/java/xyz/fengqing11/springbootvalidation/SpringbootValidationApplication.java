cf94845b1912886293c1146408750bcdcb5f0582
fengqing11 <tanzi8845@gmail.com>
2020-02-04 14:41:48 +0800

    first commit

---
+++ src/main/java/xyz/fengqing11/springbootvalidation/SpringbootValidationApplication.java
@@ -0,0 +1,13 @@
+package xyz.fengqing11.springbootvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/fengqing11/springbootvalidation/SpringbootValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.fengqing11.springbootvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
